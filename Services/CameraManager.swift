db8eee30ac89e578b5ea75dd8a13e5927c14827e
ZCM111111 <ZCM111111@users.noreply.github.com>
2026-07-18 23:12:16 +0800

    Restore CameraManager block 2: configureSession + configureCameraDevice

--- Services/CameraManager.swift
+++ Services/CameraManager.swift
@@ -21,9 +21,66 @@ final class CameraManager: NSObject, ObservableObject {
     override init() {
         self.session = AVCaptureSession()
         super.init()
+        configureSession()
     }
 
-    func startSession() { isSessionRunning = true }
-    func stopSession() { isSessionRunning = false }
+    private func configureSession() {
+        session.beginConfiguration()
+        if session.canSetSessionPreset(sessionPreset) {
+            session.sessionPreset = sessionPreset
+        }
+        guard let camera = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: cameraPosition) else {
+            session.commitConfiguration(); return
+        }
+        do {
+            let input = try AVCaptureDeviceInput(device: camera)
+            if session.canAddInput(input) {
+                session.inputs.forEach { session.removeInput($0) }
+                session.addInput(input)
+                self.videoDevice = camera
+                try configureCameraDevice(camera)
+            }
+        } catch { print("Camera input error: \(error)") }
+        if session.canAddOutput(videoOutput) {
+            session.outputs.forEach { session.removeOutput($0) }
+            session.addOutput(videoOutput)
+            videoOutput.videoSettings = [
+                kCVPixelBufferPixelFormatTypeKey as String: kCVPixelFormatType_420YpCbCr8BiPlanarFullRange
+            ]
+            videoOutput.alwaysDiscardsLateVideoFrames = true
+            videoOutput.setSampleBufferDelegate(self, queue: DispatchQueue.main)
+        }
+        session.commitConfiguration()
+    }
+
+    private func configureCameraDevice(_ device: AVCaptureDevice) throws {
+        try device.lockForConfiguration()
+        if let format = findBestFormat(for: device, targetFPS: targetFrameRate) {
+            device.activeFormat = format
+            let fpsRange = CMTimeMake(value: 1, timescale: targetFrameRate)
+            device.activeVideoMinFrameDuration = fpsRange
+            device.activeVideoMaxFrameDuration = fpsRange
+        }
+        if device.isSmoothAutoFocusSupported { device.isSmoothAutoFocusEnabled = false }
+        device.unlockForConfiguration()
+    }
+
+    private func findBestFormat(for device: AVCaptureDevice, targetFPS: Int32) -> AVCaptureDevice.Format? {
+        var bestFormat: AVCaptureDevice.Format?
+        var bestPixels: Int32 = 0
+        for format in device.formats {
+            let dims = CMVideoFormatDescriptionGetDimensions(format.formatDescription)
+            guard dims.width >= 3840 else { continue }
+            for range in format.videoSupportedFrameRateRanges {
+                if Int32(range.maxFrameRate) >= targetFPS, Int32(range.minFrameRate) <= targetFPS, dims.width > bestPixels {
+                    bestFormat = format; bestPixels = dims.width
+                }
+            }
+        }
+        return bestFormat
+    }
+
+    func startSession() { session.startRunning(); isSessionRunning = session.isRunning }
+    func stopSession() { session.stopRunning(); isSessionRunning = session.isRunning }
     func toggleCamera() {}
 }
