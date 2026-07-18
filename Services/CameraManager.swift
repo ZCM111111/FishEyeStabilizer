95cae7e142b0a89a0f5b3cf53d6251add3544849
ZCM111111 <ZCM111111@users.noreply.github.com>
2026-07-18 23:10:35 +0800

    Restore CameraManager block 1: imports + properties + AVCaptureSession init

--- Services/CameraManager.swift
+++ Services/CameraManager.swift
@@ -1,15 +1,28 @@
-import Foundation
-import CoreVideo
+@preconcurrency import AVFoundation
+@preconcurrency import CoreVideo
 import CoreMedia
+import Combine
 
 protocol CameraFrameDelegate: AnyObject {
     func cameraManager(_ manager: CameraManager, didOutputPixelBuffer pixelBuffer: CVPixelBuffer, timestamp: CMTime)
 }
 
 final class CameraManager: NSObject, ObservableObject {
-    @Published private(set) var isSessionRunning = false
-    var targetFrameRate: Int32 = 60
+    private(set) var session: AVCaptureSession
+    private(set) var videoDevice: AVCaptureDevice?
+    private let videoOutput = AVCaptureVideoDataOutput()
     weak var delegate: CameraFrameDelegate?
+    @Published private(set) var isSessionRunning = false
+    @Published var cameraPosition: AVCaptureDevice.Position = .back
+    var sessionPreset: AVCaptureSession.Preset = .hd4K3840x2160
+    var targetFrameRate: Int32 = 60
+    private var cancellables = Set<AnyCancellable>()
+
+    override init() {
+        self.session = AVCaptureSession()
+        super.init()
+    }
+
     func startSession() { isSessionRunning = true }
     func stopSession() { isSessionRunning = false }
     func toggleCamera() {}
